44c1bdc9966d5d3c3c60e08c16f4ebfdecbbea88
Wes Oldt <wes.oldt@gmail.com>
2017-10-15 16:33:28 -0400

    Added create method to dao, service, and controller.

--- src/main/java/com/wtoldt/mememagic/controller/GameApiController.java
+++ src/main/java/com/wtoldt/mememagic/controller/GameApiController.java
@@ -1,21 +1,31 @@
 package com.wtoldt.mememagic.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wtoldt.mememagic.service.GameService;
+
 @RestController
 @RequestMapping("api/v1/game")
 public class GameApiController {
-	
+
+	private GameService gameService;
+
+	@Autowired
+	public GameApiController(GameService gameService) {
+		this.gameService = gameService;
+	}
+
 	@RequestMapping("hello")
 	public String helloWorld() {
 		return "world";
 	}
-	
+
 	@RequestMapping(value="/", method=RequestMethod.POST)
-	public String createGame() {
-		return "foo";
+	public int createGame() {
+		return gameService.createGame();
 	}
 
 }
--- src/main/java/com/wtoldt/mememagic/dao/GameDao.java
+++ src/main/java/com/wtoldt/mememagic/dao/GameDao.java
@@ -1,8 +1,23 @@
 package com.wtoldt.mememagic.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Component;
+
+import com.wtoldt.mememagic.domain.Game;
 
 @Component
 public class GameDao {
+
+	private Map<Integer, Game> games = new HashMap<>();
+
+	public int createGame() {
+		final int id = games.size() + 1;
+		final Game game = new Game();
+		games.put(id, game);
+
+		return id;
+	}
 
 }
