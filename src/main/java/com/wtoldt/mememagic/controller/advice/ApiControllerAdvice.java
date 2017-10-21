19919c309db72f6903ae8ec2840622d9d5bfe2de
Wes Oldt <wes.oldt@gmail.com>
2017-10-20 23:22:12 -0400

    Made controller advice RestControllerAdvice.

--- src/main/java/com/wtoldt/mememagic/controller/advice/ApiControllerAdvice.java
+++ src/main/java/com/wtoldt/mememagic/controller/advice/ApiControllerAdvice.java
@@ -3,23 +3,35 @@ package com.wtoldt.mememagic.controller.advice;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice(basePackages= {"com.wtoldt.controller"})
+@RestControllerAdvice()
 public class ApiControllerAdvice {
 
-	@ResponseStatus(code=HttpStatus.BAD_REQUEST)
-	@ExceptionHandler(BindException.class)
-	public Map<String, Object> handleBindException() {
+	@ExceptionHandler({BindException.class})
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public Map<String, Object> handleBindException(final HttpServletRequest request, final BindException e) {
 		final Map<String, Object> response = new HashMap<>();
 
 		response.put("success", "false");
 		response.put("error", "true");
-		response.put("message", "There was a bind exception with your request.");
+
+		// TODO: better message from bindingResult
+
+		final BindingResult errors = e.getBindingResult();
+		final StringBuilder errorMessage = new StringBuilder();
+		for (FieldError fieldError : errors.getFieldErrors()) {
+			errorMessage.append(fieldError.getDefaultMessage());
+		}
+		response.put("message", errorMessage.toString());
 
 		return response;
 	}
