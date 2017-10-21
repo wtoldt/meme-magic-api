b32b9ad87f5e3599be29f789099118fa8c9d4ef6
emily-li <emily.li@live.co.uk>
2017-10-21 19:38:33 +0100

    Creating test stub for GameApiController joinGame(..)

---
+++ src/test/java/com/wtoldt/mememagic/GameApiControllerTest.java
@@ -0,0 +1,17 @@
+package com.wtoldt.mememagic;
+
+import org.junit.Test;
+
+/**
+ * Created by Emily Li on 21/10/2017.
+ */
+public class GameApiControllerTest {
+    @Test
+    public void testJoinGame() {
+        // 1. joinGame(..) should accept gameId and playerName
+        // 2. playerName should be validated (between 1 - 50 chars)
+        // 3. gameId should be validated - it needs to exist
+        // 4. gameId should be validated - it should be open
+        // 5. verify player in game
+    }
+}
