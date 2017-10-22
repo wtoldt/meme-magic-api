e768d956670a8aa518c7dbc9b7179855d04b37d7
emily-li <emily.li@live.co.uk>
2017-10-22 22:44:33 +0100

    Testing player in game after joining

--- src/test/java/com/wtoldt/mememagic/GameApiControllerTest.java
+++ src/test/java/com/wtoldt/mememagic/GameApiControllerTest.java
@@ -2,7 +2,9 @@ package com.wtoldt.mememagic;
 
 import com.wtoldt.mememagic.config.MemeMagicApiConfig;
 import com.wtoldt.mememagic.controller.GameApiController;
+import com.wtoldt.mememagic.domain.Game;
 import com.wtoldt.mememagic.domain.JoinGameRequest;
+import com.wtoldt.mememagic.domain.Player;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Emily Li on 21/10/2017.
@@ -19,6 +25,20 @@ import javax.validation.ConstraintViolationException;
 public class GameApiControllerTest {
     @Autowired
     private GameApiController gameApiController;
+
+    @Test
+    public void testJoinGame() throws Exception {
+        final String playerName = "player";
+        final int gameId = createAndJoinGame(playerName);
+
+        final Game game = gameApiController.getGame(gameId);
+        final List<Player> players = game.getPlayers();
+
+        assertEquals(1, players.size());
+        assertEquals(playerName, players.get(0).getName());
+
+        // TODO: 4. gameId should be validated - it should be open
+    }
 
     @Test(expected = ConstraintViolationException.class)
     public void testInvalidPlayerNameValidation() throws Exception {
@@ -31,24 +51,16 @@ public class GameApiControllerTest {
     }
 
     private void testJoinGameNameLimit(final int nameLength) throws Exception {
-        StringBuilder playerName = new StringBuilder();
-        for (int i = 0; i < nameLength ; i++) {
-            playerName.append('.');
-        }
+        final StringBuilder playerName = new StringBuilder();
+        IntStream.range(0, nameLength).forEach(i -> playerName.append('.'));
+        createAndJoinGame(playerName.toString());
+    }
 
+    private int createAndJoinGame(final String playerName) throws Exception {
         final int gameId = gameApiController.createGame();
         final JoinGameRequest joinGameRequest = new JoinGameRequest();
-        joinGameRequest.setPlayer(playerName.toString());
-
+        joinGameRequest.setPlayer(playerName);
         gameApiController.joinGame(gameId, joinGameRequest);
-    }
-    
-    @Test
-    public void testJoinGame() {
-        // 1. joinGame(..) should accept gameId and playerName
-        // 2. playerName should be validated (between 1 - 50 chars)
-        // 3. gameId should be validated - it needs to exist
-        // 4. gameId should be validated - it should be open
-        // 5. verify player in game
+        return gameId;
     }
 }
