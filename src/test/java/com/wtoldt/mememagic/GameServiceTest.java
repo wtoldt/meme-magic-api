91b87201faf5c914d102b71dd4902ac20064cb52
emily-li <emily.li@live.co.uk>
2017-10-29 20:15:39 +0000

    Adding tests to ensure player is correctly readied

--- src/test/java/com/wtoldt/mememagic/GameServiceTest.java
+++ src/test/java/com/wtoldt/mememagic/GameServiceTest.java
@@ -2,13 +2,18 @@ package com.wtoldt.mememagic;
 
 import com.wtoldt.mememagic.dao.GameDao;
 import com.wtoldt.mememagic.domain.Game;
+import com.wtoldt.mememagic.domain.Player;
 import com.wtoldt.mememagic.exception.GameNotJoinableException;
 import com.wtoldt.mememagic.exception.NoSuchGameException;
 import com.wtoldt.mememagic.exception.PlayerAlreadyExistsException;
 import com.wtoldt.mememagic.service.GameService;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by Emily Li on 21/10/2017.
@@ -18,17 +23,17 @@ public class GameServiceTest {
 
     @Before
     public void setup() {
-        GameDao gameDao = new GameDao();
+        final GameDao gameDao = new GameDao();
         gameService = new GameService(gameDao);
     }
 
     @Test
     public void testCreateGame() throws NoSuchGameException {
         final int gameId = gameService.createGame();
-        Game game = gameService.getGame(gameId);
+        final Game game = gameService.getGame(gameId);
 
-        Assert.assertNotNull(game);
-        Assert.assertEquals(game.getId(), gameId);
+        assertNotNull(game);
+        assertEquals(game.getId(), gameId);
     }
 
     @Test(expected = NoSuchGameException.class)
@@ -36,5 +41,34 @@ public class GameServiceTest {
         final int unknownId = 1;
         final String player = "player";
         gameService.joinGame(unknownId, player);
+    }
+
+    @Test
+    public void testPlayerNotReadyByDefault() throws Exception {
+        final int gameId = gameService.createGame();
+        final String playerName = "asdf";
+        gameService.joinGame(gameId, playerName);
+        final Game game = gameService.getGame(gameId);
+
+        final List<Player> players = game.getPlayers();
+        assertEquals(1, players.size());
+
+        final Player player = players.get(0);
+        assertEquals(false, player.isReady());
+    }
+
+    @Test
+    public void testReadyPlayer() throws Exception {
+        final int gameId = gameService.createGame();
+        final String playerName = "asdf";
+        gameService.joinGame(gameId, playerName);
+        final Game game = gameService.getGame(gameId);
+        gameService.setPlayerReady(gameId, playerName, true);
+
+        final List<Player> players = game.getPlayers();
+        assertEquals(1, players.size());
+
+        final Player player = players.get(0);
+        assertEquals(true, player.isReady());
     }
 }
