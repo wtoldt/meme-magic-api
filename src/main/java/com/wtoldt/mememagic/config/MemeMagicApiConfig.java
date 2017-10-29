4e1624b6daf5eab98169a8b79e5ff1bc07ab9c1b
emily-li <emily.li@live.co.uk>
2017-10-29 16:42:16 +0000

    Adding comment for MemeMagicApiConfig

--- src/main/java/com/wtoldt/mememagic/config/MemeMagicApiConfig.java
+++ src/main/java/com/wtoldt/mememagic/config/MemeMagicApiConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * Created initially for tests that require autowired components but do not require a full Spring Boot test
+ *
  * Created by Emily Li on 21/10/2017.
  */
 @Configuration
